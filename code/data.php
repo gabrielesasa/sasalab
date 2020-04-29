5f9898045056c5b54034b54b1f7e18a0fab52169
EC2 Default User <ec2-user@ip-172-31-39-210.ec2.internal>
2020-04-29 18:17:14 +0000

    master

--- code/data.php
+++ code/data.php
@@ -1,5 +1,36 @@
 <?php
 
-echo "{\"productList\": []}";
+$query_string = $_SERVER["QUERY_STRING"];
+parse_str($query_string,$query);
+$barcode_value = $query["barcode"]:
+
+$product0bj = new stdClass();
+$product0bj->productList = array();
+
+$db_connection = new mysqli(
+        "sasalab-1_db_1",
+        "user",
+        "password",
+        "db");
+
+if ($db_connection->connect_error){
+        die("connection failed: ". $db_connection->connection_error);
+}
+
+$results = $db_connection->query("SELECT * FROM 'item' WHERE `barcode`='L-DIN-AR01'");
+if ($results){
+        foreach($results as $row){
+            $item = new stdClass();
+            $item->name = $row["name"];
+            $item->barcode = $row["barcode"];
+            $product0bj->productList[] = $item;
+        }
+}
+// converte l'oggetto in stringa JSON
+$ productJson = json_encode ( $ productObj );
+
+// restituisce la stringa al client
+echo  $ productJson ;
+
 
 ?>
